796c173dd597581639f7b5dd67d7f889ffb5c37b
mtoulet <mkarmensky@gmail.com>
2022-09-26 14:02:39 +0200

    :construction: build: model user work in progress

--- app/model/user.js
+++ app/model/user.js
@@ -1,4 +1,28 @@
+const client = require("../config/db")
+
 class User {
+ 
+    constructor(obj){
+        this.id = obj.id;
+        this.firstname = obj.firstname;
+        this.lastname = obj.lastname;
+        this.nickname = obj.nickname;
+        this.mail = obj.mail;
+        this.password = obj.password;
+        this.trophies = obj.trophies;
+        this.honor_point = obj.honor_point;
+        this.team = obj.team;
+        this.role = obj.role;
+        this.created_at = obj.created_at;
+        this.updated_at = obj.updated_at;
+
+    }
+// create user
+   static async create(userTemp){
+       const result = await client.query(``)
+   }
+
+
 
 };
 
