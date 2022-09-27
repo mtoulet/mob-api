c74ef4af04bad44cd74667ebce93a269c532d681
Guillaume DOLLE <g.dolle12@gmail.com>
2022-09-27 14:43:14 +0200

    :building_construction: build: new routes for User

--- app/router/index.js
+++ app/router/index.js
@@ -5,4 +5,8 @@ const router = express.Router();
 // Route pour récupérer la liste de tous les profils utilisateurs
 router.get('/api/profiles', userController.getAllProfiles);
 
+router.post('/api/login', userController.login);
+
+router.post('/api/register', userController.register);
+
 module.exports = router;
