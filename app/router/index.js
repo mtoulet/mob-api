be89678c57a264ef7e5d66667b64d75960178b46
Guillaume DOLLE <g.dolle12@gmail.com>
2022-09-27 17:26:51 +0200

    :sparkles: feat: Json web token implementation

--- app/router/index.js
+++ app/router/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { user } = require('pg/lib/defaults');
 const userController = require('../controller/user');
 const router = express.Router();
+const { authenticateToken } = require('../service/jwt');
 
 // Route pour récupérer la liste de tous les profils utilisateurs
 router.get('/api/profiles', userController.getAllProfiles);
@@ -9,6 +11,11 @@ router.post('/api/login', userController.login);
 
 router.post('/api/register', userController.register);
 
-router.post('/api/disconnect', userController.disconnect)
+router.post('/api/disconnect', userController.disconnect);
+
+// Testing route for authentication
+router.get('/api/me', authenticateToken, (req, res) => {
+    res.send(req.user);
+});
 
 module.exports = router;
