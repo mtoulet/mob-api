be4009dc2792f0ad88bea1e060a70baef08e5cc8
Guillaume DOLLE <g.dolle12@gmail.com>
2022-09-29 11:57:40 +0200

    heroku test

--- app/index.js
+++ app/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use((request, response, next) => {
     response.header('Access-Control-Allow-Origin', '*');
-    // response.header('Access-Control-Allow-Credentials', true);
+    response.header('Access-Control-Allow-Credentials', true);
     response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
