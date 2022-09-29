91fdac1576859f71bcb3aa075081a4227c6a56db
Guillaume DOLLE <g.dolle12@gmail.com>
2022-09-29 11:55:46 +0200

    heroku test cors

--- app/index.js
+++ app/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
-app.use(cors({
-    origin: '*'
-}));
+// app.use(cors({
+//     origin: '*'
+// }));
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    // response.header('Access-Control-Allow-Credentials', true);
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-});
+  });
 
 /** ********* */
 /*  SWAGGER */
